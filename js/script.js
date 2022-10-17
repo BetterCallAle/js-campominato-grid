e49f83c59839f1bc970c36f340768bfaf3f30942
BetterCallAle <dap22091998@gmail.com>
2022-10-17 16:32:07 +0200

    now the game has fully interactive difficulty

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
 //extracting the DOM elements
 const playBtn = extractElements("play");
 const gridWrapper = extractElements("grid");
-
+const options = extractElements("difficulty")
 //add event click to button
 playBtn.addEventListener("click", function(){
 
-    //create 100 div with class "square" and number inside them
-    for (let i = 1; i <= 100; i++) {
-        
-        const square = createElement("square");
-        square.innerHTML = i
-        square.addEventListener("click", addActiveClass)
-        gridWrapper.append(square)
+    gridWrapper.innerHTML = ""
+
+    const userChoice = options.value
+    console.log(userChoice);
+
+    let squareNumbers = 0
+    let userDifficulty = ""
+    
+    if(userChoice === "easy"){
+        squareNumbers = 100;
+        userDifficulty = "easy"
+    } else if (userChoice === "medium"){
+        squareNumbers = 81;
+        userDifficulty = "medium"
+    } else {
+        squareNumbers = 49;
+        userDifficulty = "hard"
     }
+
+    generateGrid(squareNumbers, userDifficulty)
 })
 
 
@@ -31,6 +43,24 @@ function createElement(className) {
     return elementCreated
 }
 
+function generateGrid(numbersOfSquare, difficulty) {
+     //create 100 div with class "square" and number inside them
+     for (let i = 1; i <= numbersOfSquare; i++) {
+        
+        //create square elements
+        const square = createElement("square");
+        //add the number inside
+        square.innerHTML = i
+        //add the difficulty class
+        square.classList.add(difficulty)
+        //on click add the "active" class 
+        square.addEventListener("click", addActiveClass)
+        //stamp on the gridWrapper element
+        gridWrapper.append(square)
+    }
+}
+
+//add the class active on click for the grid elements
 function addActiveClass(){
     this.classList.toggle("active")
 }
